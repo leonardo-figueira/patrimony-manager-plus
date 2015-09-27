7b4d06c901c31b64cd45af827e546f7c2d607fea
leonardo-figueira <leonardov.figueira@hotmail.com>
2015-09-27 17:26:34 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+patrimonyManagerPlus
+====================
+
+A Symfony project created on September 27, 2015, 10:15 pm.
---
+++ src/PMPBundle/PMPBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PMPBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PMPBundle extends Bundle
+{
+}
