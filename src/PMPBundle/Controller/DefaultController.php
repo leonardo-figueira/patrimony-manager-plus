779d1002a874ef53309beb6c64727bbf4ae9fbfa
leonardo-figueira <leonardov.figueira@hotmail.com>
2015-10-10 20:06:31 -0300

    [DEV] Criado layout da pagina base

--- src/PMPBundle/Controller/DefaultController.php
+++ src/PMPBundle/Controller/DefaultController.php
@@ -9,11 +9,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/", name="index")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
+        $name = 'aqui';
         return array('name' => $name);
     }
 }
